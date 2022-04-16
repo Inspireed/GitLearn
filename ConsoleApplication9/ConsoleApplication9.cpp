11b481d9cb4004116daaf5b67695f6381f404041
Inspireed <vmeshkov312@gmail.com>
2022-04-17 00:20:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Msg {
+	string msg;
+public:
+	Msg(string msg) {
+		this->msg = msg;
+	}
+
+	virtual string GetMsg() {
+		return msg;
+	}
+};
+
+class BraketMsg : public Msg {
+public:
+	BraketMsg(string msg):Msg(msg){}
+	string GetMsg() override {
+		return "[" + ::Msg::GetMsg() + "]";
+	}
+};
+
+class Printer {
+public:
+	void Print(Msg* msg) {
+		cout << msg->GetMsg() << endl;
+	}
+};
+
+
+int main()
+{
+	Msg FromArab("Salam Aleikum");
+	Printer p;
+	p.Print(&FromArab);
+
+
+
+}
